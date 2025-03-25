697257245db3ad9f6cc3dc3669b56fdac2ad2ce4
israelgenzel <34340836+israelgenzel@users.noreply.github.com>
2025-03-25 11:59:19 +0200

    Update scraper.js

--- dist/scraper.js
+++ dist/scraper.js
@@ -9,7 +9,13 @@ async function scrape(companyId, startDate) {
             combineInstallments: false,
             showBrowser: true,
             verbose: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--headless', // מפעיל את הדפדפן במצב headless ללא ממשק גרפי
+                '--disable-gpu', // מבטל את השימוש ב-GPU
+                '--remote-debugging-port=9222' // פותח פורט לבדיקת בעיות בדפדפן
+              ],
             //executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
             //userDataDir: "C:\\Users\\Israel\\AppData\\Local\\Google\\Chrome\\User Data\\israel",
             // coockiesPath: "./cookies.txt",
