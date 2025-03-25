87aad2c8215fc25f4c4c38f9bb3b45a92ad00b1d
israelgenzel <34340836+israelgenzel@users.noreply.github.com>
2025-03-25 22:22:27 +0200

    Update db.js

--- dist/db.js
+++ dist/db.js
@@ -1,5 +1,5 @@
-const pg = require('pg');
-const { Pool } = pg;
+// const pg = require('pg');
+// const { Pool } = pg;
 // const pool = new Pool({
 //     user: 'postgres',
 //     host: 'localhost',
