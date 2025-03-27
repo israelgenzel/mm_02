8f95924e71bdf1fc69e7274cd8131598c7c90981
israelgenzel <34340836+israelgenzel@users.noreply.github.com>
2025-03-27 11:04:07 +0200

    ba

--- dist/db.js
+++ dist/db.js
@@ -1,21 +1,26 @@
-// const pg = require('pg');
-// const { Pool } = pg;
-// const pool = new Pool({
-//     user: 'postgres',
-//     host: 'localhost',
-//     database: 'mm_db',
-//     password: 'igll3573',
-//     port: 5432, // ברירת מחדל של PostgreSQL
-// });
-// module.exports = { pool };
-
- 
 const pg = require('pg');
 const { Pool } = pg;
-const pool = new Pool({
+const isPro = process.env.IS_PRO;
+let pool ;
+console.log(process.env.IS_PRO);
+if (isPro) {
+  console.log("Running locally");
+  pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'mm_db',
+    password: 'igll3573',
+    port: 5432, // ברירת מחדל של PostgreSQL
+});
+} else {
+  console.log("Running on the server");
+    pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     }
 });
+}
+
+
 module.exports = { pool };
